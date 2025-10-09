8f22a1de16d4f6a94b8135cc1234b45d1f261762
Prajwal Khobragade <prajwal.khobragade22@pccoepune.org>
2025-10-09 23:54:49 +0530

    new commit

--- backend/app/database.py
+++ backend/app/database.py
@@ -2,7 +2,7 @@ import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = os.getenv('DATABASE_URL', 'sqlite:///./carbon.db')
+DATABASE_URL = os.getenv('DATABASE_URL', 'postgresql://postgres:Mh40br7688@database-2.c49806wy8n0r.us-east-1.rds.amazonaws.com/postgres')
 engine = create_engine(
 	DATABASE_URL,
 	connect_args={"check_same_thread": False} if DATABASE_URL.startswith('sqlite') else {}
